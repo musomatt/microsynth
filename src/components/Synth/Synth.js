fe7afdc0ff9509a5aaa1d41ac1663c005386aaf2
Matt Richardson <mattrichardson@legendary-games.com>
2018-05-30 13:48:59 +0000

    Refactored the audio code out into its own module and wired up the two separate oscillators

--- src/components/Synth/Synth.js
+++ src/components/Synth/Synth.js
@@ -2,98 +2,33 @@ import React from "react";
 import SynthUI from "components/SynthUI";
 import { WAVEFORMS } from "constants/WaveConstants";
 import { NOTES } from "constants/NoteConstants";
+import AudioWrapper from "utils/AudioUtils";
 
 class Synth extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isPlaying: false,
-      oscWaveformType: WAVEFORMS.SINE
-    };
+    this.state = { isPlaying: false };
 
-    this.oscList = [];
-
-    this.arpShape = [0, 4, 7, 11, 12, 11, 7, 4];
     this.arpPos = 0;
+    this.arpShape = [0, 4, 7, 11, 12, 11, 7, 4];
 
-    this.masterGainLevel = 0.5;
-    this.masterPanLevel = 0;
+    this.toneLength = 0.19;
+    this.currentLevel = 0;
   }
 
   componentDidMount() {
-    this.setupAudioContext();
-    this.setupProcessor();
-    //this.startArpeggio();
+    this.audio = new AudioWrapper();
+    this.audio.setMasterGainLevel(0.5);
+    this.audio.setMasterPanLevel(0);
+    this.audio.setVolumeUpdate(this.onVolumeUpdate);
   }
 
-  setupAudioContext = () => {
-    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-    // master pan and gain nodes
-    this.masterPanNode = this.audioCtx.createStereoPanner();
-    this.masterGainNode = this.audioCtx.createGain();
-
-    // processor for grabbing amplitude
-    this.processor = this.audioCtx.createScriptProcessor(2048);
-    this.processor.connect(this.audioCtx.destination);
-
-    // default values
-    this.masterGainNode.gain.value = this.masterGainLevel;
-    this.masterGainNode.connect(this.masterPanNode);
-
-    this.masterPanNode.pan.value = 0;
-    this.masterPanNode.connect(this.processor);
-  };
-
-  setupProcessor = () => {
-    this.processor.onaudioprocess = this.processVolume;
-    this.processor.clipping = false;
-    this.processor.lastClip = 0;
-    this.processor.volume = 0;
-    this.processor.clipLevel = 0.98;
-    this.processor.averaging = 0.95;
-    this.processor.clipLag = 750;
-  };
-
-  processVolume = event => {
-    let x;
-    let sum = 0;
-    let inputBuffer = event.inputBuffer;
-    let outputBuffer = event.outputBuffer;
-
-    // copy all of the input channel data across to the output channels
-    for (let i = 0; i < outputBuffer.numberOfChannels; i++) {
-      var inputData = inputBuffer.getChannelData(i);
-      var outputData = outputBuffer.getChannelData(i);
-
-      for (let sample = 0; sample < inputBuffer.length; sample++) {
-        outputData[sample] = inputData[sample];
-
-        x = inputData[i];
-        sum += x * x;
-      }
-    }
-
-    // calculate level with root mean square
-    let rms = Math.sqrt(sum / inputBuffer.length);
-    let level = Math.max(rms, this.processor.volume * this.processor.averaging);
-
-    if (level !== this.state.volume) {
-      this.processor.volume = level < 0.001 ? 0 : level;
+  onVolumeUpdate = level => {
+    if (level !== this.currentLevel) {
+      this.currentLevel = level;
       this.setState({ volume: level });
     }
-  };
-
-  playTone = frequency => {
-    let osc = this.audioCtx.createOscillator();
-    osc.connect(this.masterGainNode);
-    osc.type = this.state.oscWaveformType;
-    osc.frequency.value = frequency;
-    osc.start();
-    osc.stop(this.audioCtx.currentTime + 0.19);
-
-    return osc;
   };
 
   togglePlayback = () => {
@@ -116,45 +51,38 @@ class Synth extends React.Component {
     return next;
   };
 
+  waveChange = (oscillator_id, type) => {
+    this.setState({ oscWaveformType: type });
+  };
+
+  onMasterGainChange = level => {
+    this.audio.setMasterGainLevel(level);
+  };
+
+  onMasterPanChange = level => {
+    this.audio.setMasterPanLevel(level);
+  };
+
   startArpeggio = () => {
     let note = this.getNextFrequency();
 
-    this.playTone(note);
+    this.audio.playTone(note, this.toneLength);
 
     this.playback = setInterval(() => {
       let tone = this.getNextFrequency();
 
-      this.oscList.push(this.playTone(tone));
+      this.audio.playTone(tone, this.toneLength);
     }, 200);
+
     this.setState({ isPlaying: true });
   };
 
   stopArpeggio = () => {
     clearInterval(this.playback);
 
-    this.oscList.forEach(osc => osc.stop());
-
     this.arpPos = 0;
 
     this.setState({ isPlaying: false });
-  };
-
-  waveChange = (oscillator_id, type) => {
-    this.setState({ oscWaveformType: type });
-  };
-
-  onMasterGainChange = level => {
-    if (level !== this.masterGainLevel) {
-      this.masterGainLevel = level;
-      this.masterGainNode.gain.value = level;
-    }
-  };
-
-  onMasterPanChange = level => {
-    if (level !== this.masterPanLevel) {
-      this.masterPanLevel = level;
-      this.masterPanNode.pan.value = level;
-    }
   };
 
   render() {
---
+++ src/utils/AudioUtils.js
@@ -0,0 +1,123 @@
+class AudioWrapper {
+  constructor() {
+    // setup the audio chain
+    this.audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+
+    // master audio nodes for the oscillators to talk to
+    this.masterPanNode = this.audioContext.createStereoPanner();
+    this.masterGainNode = this.audioContext.createGain();
+    this.masterProcessor = this.audioContext.createScriptProcessor(2048);
+
+    // chain of master audio nodes
+    this.masterGainNode.connect(this.masterPanNode);
+    this.masterPanNode.connect(this.masterProcessor);
+
+    this.masterProcessor.connect(this.audioContext.destination);
+    this.masterProcessor.onaudioprocess = this.processVolumeLevel;
+    this.masterProcessor.clipping = false;
+    this.masterProcessor.lastClip = 0;
+    this.masterProcessor.volume = 0;
+    this.masterProcessor.clipLevel = 0.98;
+    this.masterProcessor.averaging = 0.95;
+    this.masterProcessor.clipLag = 750;
+
+    // set default oscillator settings
+    this.oscillators = [
+      { type: "sine", gain: 1, pan: 0 },
+      { type: "square", gain: 0.5, pan: 0 }
+    ];
+
+    // list of oscillators currently playing
+    this.oscList = [];
+  }
+
+  setMasterGainLevel(level) {
+    this.masterGainNode.gain.setValueAtTime(
+      level,
+      this.audioContext.currentTime
+    );
+  }
+
+  setMasterPanLevel(level) {
+    this.masterPanNode.pan.setValueAtTime(level, this.audioContext.currentTime);
+  }
+
+  setVolumeUpdate(callback) {
+    this.processorCallback = callback;
+  }
+
+  updateOscillatorSettings(id, settings) {
+    Object.assign(this.oscillators[id], settings);
+  }
+
+  createOscillators() {
+    let oscillators = [];
+
+    for (let i = 0; i < this.oscillators.length; i++) {
+      let osc = this.audioContext.createOscillator();
+      let pan = this.audioContext.createStereoPanner();
+      let gain = this.audioContext.createGain();
+
+      // apply oscillator settings
+      osc.type = this.oscillators[i].type;
+      pan.pan.value = this.oscillators[i].pan;
+      gain.gain.value = this.oscillators[i].gain;
+
+      // build oscillator audio chain and output to the master gain
+      osc.connect(gain);
+      gain.connect(pan);
+      pan.connect(this.masterGainNode);
+
+      oscillators.push({ osc, gain, pan });
+    }
+
+    return oscillators;
+  }
+
+  playTone(freq, length) {
+    let oscillators = this.createOscillators();
+
+    // pass the frequency through all our oscillators
+    oscillators.forEach(oscillator => {
+      oscillator.osc.frequency.value = freq;
+      oscillator.osc.start();
+      oscillator.osc.stop(this.audioContext.currentTime + length);
+    });
+  }
+
+  processVolumeLevel = event => {
+    let x;
+    let sum = 0;
+    let inputBuffer = event.inputBuffer;
+    let outputBuffer = event.outputBuffer;
+
+    // copy all of the input channel data across to the output channels
+    for (let i = 0; i < outputBuffer.numberOfChannels; i++) {
+      var inputData = inputBuffer.getChannelData(i);
+      var outputData = outputBuffer.getChannelData(i);
+
+      for (let sample = 0; sample < inputBuffer.length; sample++) {
+        outputData[sample] = inputData[sample];
+
+        x = inputData[i];
+        sum += x * x;
+      }
+    }
+
+    // calculate level with root mean square
+    let rms = Math.sqrt(sum / inputBuffer.length);
+    let level = Math.max(
+      rms,
+      this.masterProcessor.volume * this.masterProcessor.averaging
+    );
+
+    this.masterProcessor.volume = level < 0.001 ? 0 : level;
+
+    if (typeof this.processorCallback === "function") {
+      this.processorCallback(this.masterProcessor.volume);
+    }
+  };
+}
+
+module.exports = AudioWrapper;
